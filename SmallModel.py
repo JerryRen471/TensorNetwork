b114ccdf9d0bdeaba6f2a97db3b35a795284dbfa
JerryRen471 <JerryMay471@gmail.com>
2023-03-13 16:46:26 +0800

    2023.03.13 added the method of calculating the ground state of the system using gradient descent

--- SmallModel.py
+++ SmallModel.py
@@ -7,6 +7,7 @@ from Tools import *
 
 class HeisenbergModel:
     def __init__(self, num_sys, J=[0, 0, 1], h=[0, 0, 1], device = 'cpu'):
+        self.device = device
         self.num_sys = num_sys
         self.H = []
         Sx = tc.tensor([[0., 1.], [1., 0.]], dtype=tc.complex64, device=device)
@@ -24,7 +25,7 @@ class HeisenbergModel:
         pass
 
     def H_function(self, phi):
-        phi_1 = tc.from_numpy(phi)
+        phi_1 = phi
         n = int(np.log2(len(phi_1)))
         shape = [2 for _ in range(n)]
         phi_1 = phi_1.reshape(shape)
@@ -35,13 +36,28 @@ class HeisenbergModel:
             phi_3 = phi_3.permute(perm)
             phi_2 += phi_3
         phi_2 += tmul(self.Hi, phi_1, pos_x=[1], pos_y=[0])
-        return phi_2.reshape(2**n, -1).numpy()
+        return phi_2.reshape(2**n, -1)
     
     def H_matmat(self, A):
-        B = np.zeros(A.shape, dtype=np.complex64)
+        B = tc.zeros(A.shape, dtype=tc.complex64)
         for i in range(A.shape[1]):
             B[:, [i]] = self.H_function(A[:, i])
         return B
+
+    def ground_state_grad(self, phi0, lr=0.01):
+        phi0.requires_grad_()
+        optimizer = tc.optim.Adam([phi0], lr=lr)
+        for _ in range(10000):
+            loss = tc.dot(phi0.conj(), self.H_function(phi0).flatten()) / (tc.dot(phi0.conj(), phi0))
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            if _%1000 == 0:
+                print('step={}: phi={}, E={}'.format(_, phi0, loss))
+        return phi0, loss
+        pass
 
 if __name__ == '__main__':
     num_sys = 3
@@ -52,16 +68,11 @@ if __name__ == '__main__':
     print(model.Hij.shape)
     phi = tc.randn(2**num_sys, dtype=tc.complex64)
     print(phi.shape)
-    H = LinearOperator(
-        shape = (2**num_sys, 2**num_sys),
-        matvec = model.H_function,
-        matmat = model.H_matmat,
-        dtype = np.complex64
-        )
-    e_vals, e_vecs = sp.linalg.eig(H.matmat(np.eye(2**num_sys, dtype=np.complex64)))
+    e_vals, e_vecs = tc.eig(model.H_matmat(tc.eye(2**num_sys, dtype=tc.complex64)), eigenvectors=True)
     print(e_vals)
     print(e_vecs[0])
     print(model.H_function(e_vecs[0]))
+    phi0, loss = model.ground_state_grad(phi0=tc.randn(2**num_sys, dtype=tc.complex64), lr=1e-3)
     # x = tc.ones([2, 3])
     # y = tc.ones([2])
     # print(x.shape, '\n', y.shape)
